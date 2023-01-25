96edc163c6fe5b99f2aae4ecfa63675c5784c9e4
Nnyeka <nnyeka212@gmail.com>
2023-01-25 00:33:54 -0800

    task_0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,7 +22,7 @@ void bubble_sort(int *array, size_t size)
 			{
 				less = array[j + 1];
 				array[j + 1] = array[j];
-				array[j] = aux;
+				array[j] = less;
 				print_array(array, size);
 			}
 		}
