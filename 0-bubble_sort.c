0d0d2c99e1aefed3f0b9164cd58db9647acecda4
Nnyeka <nnyeka212@gmail.com>
2023-01-25 00:22:35 -0800

    bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - A function that uses the bubble sort
+ * algorithm to sort an array
+ * @array: array of integers
+ * @size:  size of the array
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0;
+	int less = 0;
+
+	if (array == NULL || size == 0)
+		return;
+	for (; i < size - 1; i++)
+	{
+		for (; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				less = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = less;
+				print_array(array, size);
+			}
+		}
+	}
+}
